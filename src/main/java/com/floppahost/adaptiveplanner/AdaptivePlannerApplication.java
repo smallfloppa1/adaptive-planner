5dd8198cdf44ee0d2ba1a259f18963680e9cab42
Denys Dovgan <131907373+DenysDovgan@users.noreply.github.com>
2026-02-16 19:20:44 +0100

    Initial commit

---
+++ src/main/java/com/floppahost/adaptiveplanner/AdaptivePlannerApplication.java
@@ -0,0 +1,13 @@
+package com.floppahost.adaptiveplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdaptivePlannerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdaptivePlannerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/floppahost/adaptiveplanner/AdaptivePlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.floppahost.adaptiveplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdaptivePlannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
